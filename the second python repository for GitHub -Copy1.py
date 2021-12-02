8c51279b50815aeecb14334f56bf8e2fd5232589
hasan-firat-data-and-business-analyst <94064775+hasan-firat-data-and-business-analyst@users.noreply.github.com>
2021-12-02 16:07:54 -0500

    Add files via upload

---
+++ the second python repository for GitHub -Copy1.py
@@ -0,0 +1,328 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# First let's import the packages we will use in this project
+# You can do this all now or as you need them
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import matplotlib
+plt.style.use('ggplot')
+from matplotlib.pyplot import figure
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+matplotlib.rcParams['figure.figsize'] = (12,8)
+
+pd.options.mode.chained_assignment = None
+
+
+
+# Now we need to read in the data
+df = pd.read_csv(r'C:\Users\hasan\Downloads\movies.csv')
+
+
+# In[2]:
+
+
+# Now let's take a look at the data
+
+df
+
+
+# In[3]:
+
+
+# We need to see if we have any missing data
+# Let's loop through the data and see if there is anything missing
+
+for col in df.columns:
+    pct_missing = np.mean(df[col].isnull())
+    print('{} - {}%'.format(col, round(pct_missing*100)))
+
+
+# In[4]:
+
+
+for col in df.columns:
+    pct_missing = np.sum(df[col].isnull())
+    pct_absence = np.sum(df[col].notnull())
+    print('{} : {} of {} rows is null in the {} column'.format(col,pct_missing,pct_absence,col))
+
+
+# In[5]:
+
+
+# Data Types for our columns
+
+print(df.dtypes)
+
+
+# In[6]:
+
+
+# Are there any Outliers?
+
+df.boxplot(column=['gross'])
+
+
+# In[7]:
+
+
+df.drop_duplicates()
+
+
+# In[8]:
+
+
+# Order our Data a little bit to see
+
+df.sort_values(by=['gross'], inplace=False, ascending=False)
+
+
+# In[9]:
+
+
+sns.regplot(x="gross", y="budget", data=df)
+
+
+# In[10]:
+
+
+sns.regplot(x="score", y="gross", data=df)
+
+
+# In[11]:
+
+
+# Correlation Matrix between all numeric columns
+
+df.corr(method ='pearson')
+
+
+# In[12]:
+
+
+df.corr(method ='kendall')
+
+
+# In[13]:
+
+
+df.corr(method ='spearman')
+
+
+# In[14]:
+
+
+correlation_matrix = df.corr()
+
+sns.heatmap(correlation_matrix, annot = True)
+
+plt.title("Correlation matrix for Numeric Features")
+
+plt.xlabel("Movie features")
+
+plt.ylabel("Movie features")
+
+plt.show()
+
+
+# In[15]:
+
+
+# Using factorize - this assigns a random numeric value for each unique categorical value
+
+df.apply(lambda x: x.factorize()[0]).corr(method='pearson')
+
+
+# In[16]:
+
+
+correlation_matrix = df.apply(lambda x: x.factorize()[0]).corr(method='pearson')
+
+sns.heatmap(correlation_matrix, annot = True)
+
+plt.title("Correlation matrix for Movies")
+
+plt.xlabel("Movie features")
+
+plt.ylabel("Movie features")
+
+plt.show()
+
+
+# In[17]:
+
+
+correlation_mat = df.apply(lambda x: x.factorize()[0]).corr()
+
+corr_pairs = correlation_mat.unstack()
+
+print(corr_pairs)
+
+
+# In[18]:
+
+
+sorted_pairs = corr_pairs.sort_values(kind="quicksort")
+
+print(sorted_pairs)
+
+
+# In[19]:
+
+
+# We can now take a look at the ones that have a high correlation (> 0.5)
+
+strong_pairs = sorted_pairs[abs(sorted_pairs) > 0.5]
+
+print(strong_pairs)
+
+
+# In[20]:
+
+
+# Looking at the top 15 compaies by gross revenue
+
+CompanyGrossSum = df.groupby('company')[["gross"]].sum()
+
+CompanyGrossSumSorted = CompanyGrossSum.sort_values('gross', ascending = False)[:15]
+
+CompanyGrossSumSorted = CompanyGrossSumSorted['gross'].astype('int64') 
+
+CompanyGrossSumSorted
+
+
+# In[21]:
+
+
+df['Year'] = df['released'].astype(str).str[:4]
+df
+
+
+# In[22]:
+
+
+df.groupby(['company', 'year'])[["gross"]].sum()
+
+
+# In[23]:
+
+
+CompanyGrossSum = df.groupby(['company', 'year'])[["gross"]].sum()
+
+CompanyGrossSumSorted = CompanyGrossSum.sort_values(['gross','company','year'], ascending = False)[:15]
+
+CompanyGrossSumSorted = CompanyGrossSumSorted['gross'].astype('int64') 
+
+CompanyGrossSumSorted
+
+
+# In[24]:
+
+
+CompanyGrossSum = df.groupby(['company'])[["gross"]].sum()
+
+CompanyGrossSumSorted = CompanyGrossSum.sort_values(['gross','company'], ascending = False)[:15]
+
+CompanyGrossSumSorted = CompanyGrossSumSorted['gross'].astype('int64') 
+
+CompanyGrossSumSorted
+
+
+# In[25]:
+
+
+plt.scatter(x=df['budget'], y=df['gross'], alpha=0.5)
+plt.title('Budget vs Gross Earnings')
+plt.xlabel('Gross Earnings')
+plt.ylabel('Budget for Film')
+plt.show()
+
+
+# In[26]:
+
+
+df
+
+
+# In[27]:
+
+
+df_numerized = df
+
+
+for col_name in df_numerized.columns:
+    if(df_numerized[col_name].dtype == 'object'):
+        df_numerized[col_name]= df_numerized[col_name].astype('category')
+        df_numerized[col_name] = df_numerized[col_name].cat.codes
+        
+df_numerized
+
+
+# In[28]:
+
+
+df_numerized.corr(method='pearson')
+
+
+# In[29]:
+
+
+correlation_matrix = df_numerized.corr(method='pearson')
+
+sns.heatmap(correlation_matrix, annot = True)
+
+plt.title("Correlation matrix for Movies")
+
+plt.xlabel("Movie features")
+
+plt.ylabel("Movie features")
+
+plt.show()
+
+
+# In[30]:
+
+
+for col_name in df.columns:
+    if(df[col_name].dtype == 'object'):
+        df[col_name]= df[col_name].astype('category')
+        df[col_name] = df[col_name].cat.codes
+
+
+# In[32]:
+
+
+df
+
+
+# In[36]:
+
+
+sns.swarmplot(x="rating", y="gross", data=df)
+
+
+# In[37]:
+
+
+sns.stripplot(x="rating", y="gross", data=df)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
